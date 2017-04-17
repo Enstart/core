40046196fc3b299c2f4d6b441cdf066f574efcf3
Magnus Eriksson <mange@reloop.se>
2017-04-17 13:54:25 +0200

    Fixed some bugs

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -5,13 +5,23 @@ use Enstart\Exception\UnknownPropertyException;
 
 class Controller
 {
+    /**
+     * @var ContainerInterface
+     */
     protected static $container = [];
 
+    /**
+     * @param ContainerInterface $container
+     */
     public static function setContainer(ContainerInterface $container)
     {
         static::$container = $container;
     }
 
+    /**
+     * @param  string $property
+     * @return mixed
+     */
     public function __get($property)
     {
         if (!static::$container->isAlias($property)) {
@@ -21,9 +31,35 @@ class Controller
         return static::$container->make($property);
     }
 
+    /**
+     * Call a method on the main App instance
+     *
+     * @param  string $method
+     * @param  array  $args
+     * @return mixed
+     */
     public function __call($method, array $args)
     {
         $app = static::$container->make('Enstart\App');
         return call_user_func_array([$app, $method], $args);
+    }
+
+    /**
+     * Redirect to a named route
+     *
+     * @param  string  $name
+     * @param  string  $args  Route arguments
+     * @param  integer $code
+     * @param  array   $headers
+     *
+     * @return Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    protected function routeRedirect($name, array $args = [], $code = 302, array $headers = [])
+    {
+        return $this->response->redirect(
+            $this->router->getRoute($name, $args),
+            $code,
+            $headers
+        );
     }
 }
--- src/View/ViewExtension.php
+++ src/View/ViewExtension.php
@@ -1,6 +1,6 @@
 <?php namespace Enstart\View;
 
-use Enstart\Router\RouterInterface;
+use Enstart\App;
 use League\Plates\Engine;
 use League\Plates\Extension\ExtensionInterface;
 use League\Plates\Extension\URI;
@@ -15,14 +15,14 @@ class ViewExtension implements ExtensionInterface
     /**
      * @var RouterInterface
      */
-    protected $router;
+    protected $app;
 
     /**
-     * @param RouterInterface $router
+     * @param Enstart\App $app
      */
-    public function __construct(RouterInterface $router)
+    public function __construct(App $app)
     {
-        $this->router = $router;
+        $this->app = $app;
     }
 
     /**
@@ -34,8 +34,36 @@ class ViewExtension implements ExtensionInterface
     {
         $this->engine = $engine;
 
-        $this->engine->loadExtension(new URI($this->router->getRequestPath()));
+        $this->engine->loadExtension(
+            new URI($this->app->router->getRequestPath())
+        );
+
+        $engine->registerFunction('asset', [$this, 'asset']);
         $engine->registerFunction('route', [$this, 'route']);
+        $engine->registerFunction('excerpt', [$this, 'excerpt']);
+        $engine->registerFunction('queryString', [$this, 'queryString']);
+    }
+
+    /**
+     * Append file modification timestamp for an asset
+     *
+     * @param  string $file
+     * @return string
+     */
+    public function asset($file)
+    {
+        if (!$this->app->path('public')) {
+            return $file;
+        }
+
+        $public   = rtrim($this->app->path('public'), '/');
+        $filepath = $public . '/' . ltrim($file, '/');
+
+        if (!is_file($filepath)) {
+            return $file;
+        }
+
+        return $file . '?' . filemtime($filepath);
     }
 
     /**
@@ -47,6 +75,64 @@ class ViewExtension implements ExtensionInterface
      */
     public function route($name, array $args = [])
     {
-        return $this->router->getRoute($name, $args);
+        return $this->app->router->getRoute($name, $args);
+    }
+
+    /**
+     * Get an excerpt of a text string
+     *
+     * @param  string  $text
+     * @param  integer $maxLength
+     * @param  string  $suffix
+     * @return string
+     */
+    public function excerpt($text, $maxLength = 300, $suffix = '...')
+    {
+        // Check if the body has a user defined "<!--more-->"-tag
+        $more = stripos($text, '<!--more-->');
+        if ($more !== false) {
+            // We found a tag, use that position for the excertip
+            // instead of the default
+            return strip_tags(substr($text, 0, $more));
+        }
+
+        $text = strip_tags($text);
+
+        if(strlen($text) > $maxLength) {
+            $text      = substr($text, 0, $maxLength - strlen($suffix));
+            $lastSpace = strrpos($text, ' ');
+            $text      = substr($text, 0, $lastSpace);
+            $text      .= $suffix;
+        }
+
+        return $text;
+    }
+
+
+    /**
+     * Get the current query string
+     *
+     * @param  array  $add
+     * @param  array  $remove
+     * @return string
+     */
+    public function queryString(array $add = [], array $remove = [])
+    {
+        $qs     = $this->app->request->server('QUERY_STRING');
+        $values = [];
+
+        if ($qs) {
+            parse_str($qs, $values);
+        }
+
+        foreach ($remove as $rmKey) {
+            if (array_key_exists($rmKey, $values)) {
+                unset($values[$rmKey]);
+            }
+        }
+
+        $values = array_replace($values, $add);
+
+        return '?' . http_build_query($values);
     }
 }
